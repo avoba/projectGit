d7a136256ba77655291cedcbea240666225deb22
paula_pacheco <paula_pacheco@10.1.48.221>
2013-09-30 08:44:47 -0300

    inicio do projeto

---
+++ src/testegit/TesteGit.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testegit;
+
+/**
+ *
+ * @author paula_pacheco
+ */
+public class TesteGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("teste");
+    }
+}
