e718817ab1a031fce6515d5d5cda12236c59fe53
paula_pacheco <paula_pacheco@10.1.48.221>
2013-09-30 08:55:00 -0300

    adicionando classe pessoa

---
+++ src/testegit/Pessoa.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testegit;
+
+/**
+ *
+ * @author paula_pacheco
+ */
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    
+    
+}
